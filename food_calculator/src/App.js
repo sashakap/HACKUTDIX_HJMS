2a961b8f3bda6b8107dd57838a4fd4831c82c53f
Jacob-Wiedemeier <jsw190002@utdallas.edu>
2022-11-12 22:58:02 -0600

    Basic aggregator example

--- food_calculator/src/App.js
+++ food_calculator/src/App.js
@@ -11,8 +11,26 @@ function App() {
   const [cart, setCart] = useState([]);
   const [input, setInput] = useState('');
   const term = input;
+
   const q=query(collection(db,'items'), where('name', '>=', cleanQuery(term)), where('name', '<=', cleanQuery(term) + '~'), orderBy('name'));
   useEffect(() => {onSnapshot(q,(snapshot)=>{setItems(snapshot.docs.map(doc=>doc.data()))})});
+
+  let itemEnergyAggregate = 0;
+  let itemProteinAggregate = 0;
+  let itemCarbAggregate = 0;
+  let itemFatAggregate = 0;
+  for(let i = 0; i < items.length; i++){
+    // parseInt || 0 indicates to assume 0 if parseInt fails
+    itemEnergyAggregate += parseInt(items[i].energy) || 0;
+    itemProteinAggregate += parseInt(items[i].protein) || 0;
+    itemCarbAggregate += parseInt(items[i].carbohydrate) || 0;
+    itemFatAggregate += parseInt(items[i].fat) || 0;
+  }
+  const itemAggregate = ["Energy: " + itemEnergyAggregate + "kcal", 
+                        "Protein: " + itemProteinAggregate + "g",
+                        "Carbohydrates: " + itemCarbAggregate + "g",
+                        "Fat: " + itemFatAggregate + "g"];
+
   const search=(e)=>{e.preventDefault(); setInput(input); setInput('')};
   return (
     <div className="App">
@@ -27,6 +45,15 @@ function App() {
             </div>
           </div>
           <div className="Calculator">
+            <p>
+              {itemAggregate[0]}
+              <br></br>
+              {itemAggregate[1]}
+              <br></br>
+              {itemAggregate[2]}
+              <br></br>
+              {itemAggregate[3]}
+            </p>
            <CalculatorLabel/>
           </div>
       </header>
