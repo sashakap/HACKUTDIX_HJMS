c0c3afcdc2263f7cc3a5a61ebe189d93b7437ca8
Alexander Kaplan <alexandermikaplan@hotmail.com>
2022-11-12 20:58:38 -0600

    Merge Conflict Resolution

--- food_calculator/src/App.js
+++ food_calculator/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
 import { CalculatorLabel } from './Components/CalculatorLabel';
-=======
 import React, {useState, useEffect} from 'react';
-import { CalculatorLabel } from './Components/CalculatorLabel';
 import { db } from './firebase.js';
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
->>>>>>> a77065f7b880a2b1f079c4042be703720fbfbfc8
+
+
 
 const q=query(collection(db,'items'),orderBy('name'));
 function App() {
@@ -14,20 +12,18 @@ function App() {
   useEffect(() => {onSnapshot(q,(snapshot)=>{setItems(snapshot.docs.map(doc=>doc.data()))})});
   return (
     <div className="App">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Commissioner:wght@100&family=Maven+Pro&display=swap');
+      </style>
       <header className="App-header">
          <div className='categories'>
             <div className ="searchSection">
               <h1>What Are you in the Mood For?</h1>
-<<<<<<< HEAD
               <div className='searchbar'>
                 <input type="text" className="foodSearch"></input>
-                <button className='searchButton'></button>
               </div>
-=======
-              <input type="foodSearch" name="fname"></input>
-              <searchvar></searchvar>
+              <searchbar></searchbar>
               <ul>{items.map(item => <li>Name: {item.name}, Energy: {item.energy}</li>)}</ul>
->>>>>>> a77065f7b880a2b1f079c4042be703720fbfbfc8
             </div>
           </div>
           <div className="Calculator">
